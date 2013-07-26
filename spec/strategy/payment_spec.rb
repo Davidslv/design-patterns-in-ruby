b89b916c287d8d657fa988d11281532be93ee651
Tiago Scolari <tscolari@housetrip.com>
2013-07-26 18:37:02 +0100

    Some proposed refactorings :)

--- spec/strategy/payment_spec.rb
+++ spec/strategy/payment_spec.rb
@@ -1,29 +1,31 @@
 require 'spec_helper'
 
 require 'strategy/order'
+require 'strategy/payment'
 require 'strategy/payment_strategy'
 require "strategy/strategies/cash_payment_strategy"
 require "strategy/strategies/credit_card_payment_strategy"
 require "strategy/strategies/paypal_payment_strategy"
 
 
-describe PaymentStrategy do
+#describe PaymentStrategy do
+describe Payment do
 
   it "should process a cash payment" do
     order   = Order.new
-    payment = described_class.create(order)
-    payment.process(order).should == "Cash payment accepted"
+    payment = described_class.new(order)
+    payment.process.should == "Cash payment accepted"
   end
   
   it "should process a credit card payment" do
     order   = Order.new(:payment_method => :credit_card)
-    payment = described_class.create(order)
-    payment.process(order).should == "Credit Card order processed"
+    payment = described_class.new(order)
+    payment.process.should == "Credit Card order processed"
   end
   
   it "should process a paypal payment" do
     order   = Order.new(:payment_method => :paypal)
-    payment = described_class.create(order)
-    payment.process(order).should == "Paypal order processed"
+    payment = described_class.new(order)
+    payment.process.should == "Paypal order processed"
   end
 end
